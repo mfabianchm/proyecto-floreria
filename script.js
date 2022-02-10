be3ebbdcf796fc1eb3445c0d2265d27d7c9a869c
Marcos Chong <mfchongm@hotmail.com>
2022-02-09 21:47:09 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+//SCRIPT PARA NAV Y HERO
+
+const menuBarsIcon = document.querySelector('.menu-bars-mobile')
+const exitIconMobile = document.querySelector('.exit-icon-mobile')
+const heroElementsOpacity = document.querySelector('.opacity-hero')
+
+const menu_Links_Mobile = document.querySelector('.links-container-mobile')
+
+menuBarsIcon.addEventListener('click', e => {
+    menuBarsIcon.style.display = 'none'
+    exitIconMobile.style.display = 'inline-block'
+    menu_Links_Mobile.classList.add('links-container-mobile-appear')
+    heroElementsOpacity.classList.add('opacity-click')
+})
+
+exitIconMobile.addEventListener('click', e => {
+    exitIconMobile.style.display = 'none'
+    menuBarsIcon.style.display = 'inline-block'
+    menu_Links_Mobile.classList.remove('links-container-mobile-appear')
+    heroElementsOpacity.classList.remove('opacity-click')
+})
+
+
+
